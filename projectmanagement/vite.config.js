183b0120c825791b387b761bb55e5ca321d6c2ee
Fanni Wihl <fanniwihl@gmail.com>
2024-02-26 19:16:09 +0100

    fixing deploy issue

--- projectmanagement/vite.config.js
+++ projectmanagement/vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/project-management-app/'
+  base: '/project-management-app/',
+  build: {
+    outDir: 'build'
+  }
 })
