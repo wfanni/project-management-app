f98b5bae550eb3097fd5ded2f03d04027d567ba6
Fanni Wihl <fanniwihl@gmail.com>
2024-02-27 12:17:13 +0100

    config changes

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/project-management-app',
+  base: './',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
