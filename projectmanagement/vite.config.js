fee7b04757c4b13adff6c945aa735abdafd26d97
Fanni Wihl <fanniwihl@gmail.com>
2024-02-27 11:32:35 +0100

    config files modifications

--- projectmanagement/vite.config.js
+++ projectmanagement/vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/project-management-app/',
+  base: '/project-management-app',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   build: {
     outDir: 'build'
   }
